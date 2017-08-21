bd219043f958e15f8ab17a12e04b3bb16a7f67b4
cba <theodor.will@gmail.com>
2017-08-21 16:53:44 +0200

    Icon added
    Files made private for safer code
    
    Response added for 0 results search

--- app/src/main/java/com/example/cba/tvshowsapp/MainActivity.java
+++ app/src/main/java/com/example/cba/tvshowsapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -26,11 +29,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String searchUrl = "/singlesearch/shows?q=";
-    public static final String embedUrl = "&embed=episodes";
-    public String searchResult = "";
+    private static final String searchUrl = "/singlesearch/shows?q=";
+    private static final String embedUrl = "&embed=episodes";
+    private String searchResult = "";
     private RecyclerView recyclerView;
-    private Button confirm;
     private EditText searchField;
     private TextView showName;
 
@@ -38,14 +40,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
-        confirm = (Button)findViewById(R.id.confirmButton);
         searchField = (EditText)findViewById(R.id.searchEdit);
-        confirm = (Button)findViewById(R.id.confirmButton);
         showName = (TextView)findViewById(R.id.showName);
 
-        confirm.setOnClickListener(new View.OnClickListener(){
+        searchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-            public void onClick(View v) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
                 searchResult = searchField.getText().toString();
                 fetchSearchResults();
             }
@@ -74,7 +84,13 @@ public class MainActivity extends AppCompatActivity {
                     String nameOfShow = response.body().getName();
                     showName.setText(nameOfShow);
             } else {
-                    Toast.makeText(getApplicationContext(), "No result found for "+searchResult, Toast.LENGTH_SHORT).show();
+                    if(searchResult.equals("")) {
+                        recyclerView.setAdapter(null);
+                        showName.setText("");
+                    } else{
+                        recyclerView.setAdapter(null);
+                        showName.setText("No results found");
+                    }
                 }
             }
             @Override
